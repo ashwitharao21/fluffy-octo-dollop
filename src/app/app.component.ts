9243501879ac923ebd1746a97fdab3ae5ed55f9f
Ashwitha A Rao <ashwitharao366@gmail.com>
2020-12-19 13:20:49 +0530

    CRUD Operation Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,91 +9,90 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-  msg: boolean;
+  deleteMsg: boolean;
+  updateMsg: boolean;
   success: boolean;
-
-
-  userRecords: any[] = [];
-  fetchEmployeeSubscriber: any;
-  collabcontent: any;
-  _url = 'http://mspr2web.vlabsinc.com/';
+  msg: boolean;
+  allPosts = [];
 
   constructor(private _http: HttpClient,) { }
-
-
   
   ngOnInit(): void {
+
+    this.getPost();
     
-    this.fetchEmployee();
-   
   }
 
-  private fetchEmployee() {
-    // debugger
-    // Create - post API
-    // Read - get
-    // Update - put
-    // Delete - delete
-    // 
-     this.fetchEmployeeSubscriber = this._http.get(this._url + 'api/doc')
-    .subscribe((res: any) => {
-        // console.log(res);
-        this.userRecords.push(res);
-        // console.log(this.userRecords);
-        // debugger
-        this.collabcontent = this.userRecords[0]._meta.url;
-        console.log(this.collabcontent)
-
-    });
-   /*  let id = 0;
-    this._http.get(this._url + '/api/tenant/details/' + id)
-    .subscribe((res: any) => {
-        // console.log(res);
-        this.userRecords.push(res);
-        // console.log(this.userRecords);
-        // debugger
-
-    }); */
-  }
-
-  fetchCollabcontent(form: NgForm){
+  updatePost(form: NgForm) {
     const value = form.value;
-    console.log(value);
-    
     const data = {
-      company_type: value.company_type,
-      segment_id: value.segment_id,
-      industry_category: value.industry_category,
-      industry_subcategory: value.industry_subcategory,
-      service_type: value.service_type,
-      revenue_range: value.revenue_range,
-      employee_range: value.employee_range,
-      years_in_business: value.years_in_business,
-      lifestage: value.lifestage,
-      country_code: value.country_code,
-      active: value.active,
-      name: value.name,
-      website_url: value.website_url,
-      address_line1: value.address_line1,
-      address_line2: value.address_line2,
-      city: value.city,
-      state: value.state,
-      country: value.country,
-      zip: value.zip,
+      title: value.title,
+      body: value.body,
+      userId: 1
     }
     let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
-    this.fetchEmployeeSubscriber = this._http.post(this._url + 'api/company/company-create', data,  { headers: headers })
+        'Content-type': 'application/json; charset=UTF-8',
+    })
+    this._http.put('https://jsonplaceholder.typicode.com/posts/1', data, {headers: headers})
     .subscribe((res: any) => {
         console.log(res);
-        // this.userRecords.push(res);
-        // console.log(this.userRecords);
-        
-        // this.collabcontent = this.userRecords[0]._meta.url;
-        // console.log(this.collabcontent)
-
-    },(err: any) => {console.log(err)});
+        this.allPosts.pop();
+        this.allPosts.push(res);
+        this.updateMsg = true;
+        this.msg = false;
+        this.success = false;
+        this.deleteMsg = false;
+    });
   }
 
+  deletePost() {
+    this._http.delete('https://jsonplaceholder.typicode.com/posts/1')
+    .subscribe((res: any) => {
+        console.log(res);
+        this.allPosts.pop();
+        this.deleteMsg = true;
+        this.msg = false;
+        this.success = false;
+        this.updateMsg = false;
+
+    });
+  }
+
+  createPost(form: NgForm) {
+    const value = form.value;
+    const data = {
+      title: value.title,
+      body: value.body,
+      userId: 1
+    }
+    let headers = new HttpHeaders({
+      'Content-type': 'application/json; charset=UTF-8',
+    })
+  this._http.post('https://jsonplaceholder.typicode.com/posts/', data, {headers: headers})
+    .subscribe((res: any) => {
+        this.allPosts.push(res)
+        console.log(res);
+        this.success = true;
+        this.deleteMsg = false;
+        this.updateMsg = false;
+        this.msg = false;
+    }, (err) => {
+      this.msg = true;
+      this.success = false;
+      this.deleteMsg = false;
+      this.updateMsg = false;
+     });
+  }
+
+  getPost() {
+    this._http.get('https://jsonplaceholder.typicode.com/posts/')
+    .subscribe((res: any) => {
+      this.allPosts = res;
+        console.log(res);
+        this.msg = false;
+        this.success = false;
+        this.deleteMsg = false;
+        this.updateMsg = false;
+    });
+  }
 }
